ca8714084ffa1665224185afd035b25313c8be00
gamegine <victor.fromentin95@ynov.com>
2019-10-18 13:12:40 +0200

    favorite AsyncStorage save

--- page/AddFavorite.js
+++ page/AddFavorite.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, AsyncStorage } from 'react-native';
 
 export default class Favorite extends React.Component {
   state = { input: '' }
-  
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,16 +11,23 @@ export default class Favorite extends React.Component {
           style={{ borderColor: 'gray', borderWidth: 1 }}
           onChangeText={text => this.onChangeText(text)}
         />
-        <Button title="Add" onPress={() => this.onAdd()}/>
+        <Button title="Add" onPress={() => this.onAdd()} />
       </View>
     );
   }
   onChangeText(text) {
     this.setState({ input: text })
   }
-  onAdd(){
-    console.log('add fav');
-    this.props.navigation.goBack();
+  onAdd() {
+    console.log('add fav', this.state.input);
+    AsyncStorage.getItem('city').then(data=>{
+      console.log('current fav :',data)
+      city = []
+      if(data!=null){city=JSON.parse(data)}
+      city.push(this.state.input);
+      console.log('new fav',city)
+      AsyncStorage.setItem('city', JSON.stringify(city)).then(()=>{this.props.navigation.goBack()})
+    })
   }
 };
 
