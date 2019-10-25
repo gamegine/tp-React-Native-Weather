98e1e5577e8d6c184080391bee47ad3e6e1db5c7
gamegine <victor.fromentin95@ynov.com>
2019-10-25 16:00:17 +0200

    setup redux

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigation';
+import {Provider} from 'react-redux'
+import Store from './store/configureStore'
 
 export default class App extends React.Component {
-  render() { return (<AppNavigator/>); }
+  render() { return (<Provider store={Store}><AppNavigator/></Provider>); }
 }
---
+++ store/configureStore.js
@@ -0,0 +1,4 @@
+import { createStore } from 'redux'
+import Weather from './reducers/Weather'
+
+export default createStore(Weather)
