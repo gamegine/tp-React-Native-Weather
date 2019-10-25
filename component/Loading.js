439aa3cf7f2060293f39610304548cdc4fb65f47
gamegine <victor.fromentin95@ynov.com>
2019-10-25 11:06:26 +0200

    loader add ActivityIndicator

--- component/Loading.js
+++ component/Loading.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class Loading extends React.Component {
   render()
   {
     return (
       <View style={styles.container}>
+        <ActivityIndicator/>
         <Text>Loading ...</Text>
       </View>
     );
