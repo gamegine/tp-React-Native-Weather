84a00145a56d1ec9ff24c57a3029d9cdc1ba6ff1
gamegine <victor.fromentin95@ynov.com>
2019-10-25 11:02:10 +0200

    fav show temp

--- component/FavoriteItem.js
+++ component/FavoriteItem.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Text, TouchableHighlight } from 'react-native';
+import { Text, View, TouchableHighlight } from 'react-native';
 import Swipeout from 'react-native-swipeout';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+
+import Weather from '../service/Weather'
 
 export default class FavoriteItem extends React.Component {
   prototype = {
@@ -9,11 +12,25 @@ export default class FavoriteItem extends React.Component {
     onRemove: PropTypes.func.isRequired,
     th: PropTypes.instanceOf.isRequired
   }
+
+  service = new Weather();
+  state = { data: null };
+  componentDidMount() {
+    this.service.getWeather(this.props.name).then((response) => {
+      console.log(response.data)
+      this.setState({ data: response.data })
+    })
+  }
+
+
   render() {
     return (
-      <Swipeout right={[{ text: 'Remove', backgroundColor: 'red', onPress: () => { this.props.onRemove(this.props.name,this.props.th) } }]}>
+      <Swipeout right={[{ text: 'Remove', backgroundColor: 'red', onPress: () => { this.props.onRemove(this.props.name, this.props.th) } }]}>
         <TouchableHighlight onPress={() => this.props.navigation.push('DetailFavorite', { "city": this.props.name })}>
-          <Text style={{ fontSize: 23 }}>{this.props.name}</Text>
+          <View>
+            <Text style={{ fontSize: 23 }}>{this.props.name}</Text>
+            {this.state.data != null ? <Text style={{textAlign:"center"}}>temp: {this.state.data.main.temp}°</Text> : <Loading />}
+          </View>
         </TouchableHighlight>
       </Swipeout>
     );
--- component/Loading.js
+++ component/Loading.js
@@ -15,7 +15,6 @@ export default class Loading extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
