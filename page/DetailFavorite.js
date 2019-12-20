635e76506bc2c78c44f09d76b764eb2ae043017d
gamegine <gamegine@protonmail.com>
2019-12-20 13:05:19 +0100

    detail favorite nav option title = name

--- page/DetailFavorite.js
+++ page/DetailFavorite.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Weather from '../component/Weather'
 
 export default class DetailFavorite extends React.Component {
+  static navigationOptions = ({ navigation }) => ({title: navigation.getParam("city")});
   render() {
     return (
       <Weather city={this.props.navigation.getParam("city")} data={this.props.navigation.getParam("data")} />
